3f90efa79e037972113a408c22af250d9d3d8c66
DOB <79879986+alblymlk@users.noreply.github.com>
2021-03-15 12:11:38 +0300

    Update DOB.lua

--- DOB.lua
+++ DOB.lua
@@ -10570,7 +10570,7 @@ database:set(bot_id.." amir:age_Bots"..msg.chat_id_,"open")
 end
 if text and text:match("^احسب (.*)$") and database:get(bot_id.." amir:age_Bots"..msg.chat_id_) == "open" then
 local Textage = text:match("^احسب (.*)$")
-ge = https.request('https://rudi-dev.tk/Amir3/DOB.php?age='..URL.escape(Textage)..'')
+ge = https.request('https://rudi-dev.tk/Amir3/BOYKA.php?age='..URL.escape(Textage)..'')
 ag = JSON.decode(ge)
 i = 0
 for k,v in pairs(ag.ok) do
